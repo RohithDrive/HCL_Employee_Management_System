3a2a072707cf2d4d42cc5577d81a77135d9add7c
RohithDrive <malapatirohith666@gmail.com>
2026-04-07 12:46:08 +0530

    Auto Deploy to Tomcat Server

--- backend/src/main/java/com/hcl/HCLEmployee_Management_System/HclEmployeeManagementSystemApplication.java
+++ backend/src/main/java/com/hcl/HCLEmployee_Management_System/HclEmployeeManagementSystemApplication.java
@@ -2,9 +2,16 @@ package com.hcl.HCLEmployee_Management_System;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HclEmployeeManagementSystemApplication {
+public class HclEmployeeManagementSystemApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(HclEmployeeManagementSystemApplication.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(HclEmployeeManagementSystemApplication.class, args);
